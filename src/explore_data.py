9fad725e25206e8ced63cb10549cea5dce1d04a6
PunifX <hanniouiyassine.info@gmail.com>
2026-04-15 20:51:45 +0100

    fix: update type mapping for df1 and ensure consistent data structure in explore_data.py

--- src/explore_data.py
+++ src/explore_data.py
@@ -10,5 +10,20 @@ lf1 = lf1.drop_duplicates().dropna(subset=['url'])
 
 df1 = df1[['url','type']].copy()
 
+df1['type'] = df1['type'].map({
+    'benign' : 'benign',
+    'phishing': 'phishing',
+    'defacement': 'phishing',
+    'malware': 'phishing',
+})
+
+
 df2 = df2[['url']].copy()
 df2['type'] = 'phishing'
+
+lf1 = lf1[['url']].copy()
+lf1['type'] = 'benign'
+
+print(df1['type'].value_counts(normalize=True),"\t",len(df1))
+print(df2['type'].value_counts(normalize=True),"\t",len(df2))
+print(lf1['type'].value_counts(normalize=True),"\t",len(lf1))
