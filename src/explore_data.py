5af704ab16b42c645822de97d52a9986d35015e8
PunifX <hanniouiyassine.info@gmail.com>
2026-04-15 20:38:28 +0100

    fix: enhance data cleaning by adding dropna and copying relevant columns in explore_data.py

--- src/explore_data.py
+++ src/explore_data.py
@@ -4,6 +4,11 @@ df1 = pd.read_csv('data/malicious_phish.csv')
 df2 = pd.read_csv('data/malicious_phish2.csv')
 lf1 = pd.read_csv('data/legit_sites.csv')
 
-df1 = df1.drop_duplicates()
-df2 = df2.drop_duplicates()
-lf1 = lf1.drop_duplicates()
+df1 = df1.drop_duplicates().dropna(subset=['url'])
+df2 = df2.drop_duplicates().dropna(subset=['url'])
+lf1 = lf1.drop_duplicates().dropna(subset=['url'])
+
+df1 = df1[['url','type']].copy()
+
+df2 = df2[['url']].copy
+df2.clean['type'] = 'phishing'
