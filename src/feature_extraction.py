53f27e6e3e984838d979437e300760912b98e904
PunifX <hanniouiyassine.info@gmail.com>
2026-04-16 15:26:48 +0100

    fix: refactor feature extraction logic and correct variable initialization in feature_extraction.py

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import re
-counter_dots = 0
-counter_numbers = 0
+from urllib.parse import urlparse
+import requests
+
+
 df = pd.read_csv('data/final_dataset.csv')
 
 def extract_features(url):
+
+    counter_dots = 0
+    counter_numbers = 0
+    counter_paths = 0
+    starts_with_https = False
+
     features = {}
 
     features ['url_length'] = len(url)
@@ -12,20 +20,40 @@ def extract_features(url):
 
     for i in url:  
         if i == '.':
-            counter_dots+=1
+            counter_dots = counter_dots + 1
         if i.isdigit():
-            counter_numbers+=1
+            counter_numbers= counter_numbers+ 1
+        if i == '/':
+            counter_paths=counter_paths + 1
+            
 
     features ['num_dots']  = counter_dots
 
-    features['num_digitals'] = counter_numbers
+    features['num_digits'] = counter_numbers
 
-    features['has_https']
+    features['num_paths'] = counter_paths
 
-    features['has_ip']
+    ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
 
-    features['num_subdomains']
+    features['is_an_ip'] = 1 if re.search(ip_pattern, url) else 0
 
-    features['num_paths']
+    parsed = urlparse(url)
+    #print(parsed)
+    
+    hostname = parsed.netloc
 
+    if hostname == "":
+        features['num_subdomains'] = 0
+        return features
+    else:
+        parts = hostname.split('.')
+        final = max(0,len(parts) - 2)
+
+    features['num_subdomains'] = final
+
+    
     return features
+    
+url = str(input())
+features = extract_features(url)
+print(features)
